bbff7b1ad91aebf2025fb7f564e014ea078d45b5
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-17 18:44:42 +0200

    Create StatusMessage.jsx

---
+++ Tetris/src/containers/StatusMessage.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { connect } from "react-redux";
+import { getMessage } from "../stores/message";
+
+import ModalText from "../components/ModalText";
+
+const mapStateToProps = (state) => {
+  const { text, subtext } = getMessage(state);
+  return {
+    text,
+    subtext,
+  };
+};
+
+const StatusMessage = (props) => <ModalText {...props} />;
+
+export default connect(mapStateToProps)(StatusMessage);
